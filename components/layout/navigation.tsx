e3826ddf9b0ba9bc1dc0ef0b2f301ee5af294939
DvoiD <mahmoud.tayeb@outlook.com>
2026-02-14 02:55:04 +0300

    Update navigation link label to "VoiD Tech"

--- components/layout/navigation.tsx
+++ components/layout/navigation.tsx
@@ -53,7 +53,7 @@ const Navigation: FC = () => {
 							href="/"
 							className="text-xl font-bold tracking-tighter text-foreground hover:text-primary transition-colors"
 						>
-							DvoiD
+							VoiD Tech
 						</Link>
 
 						{/* Desktop Navigation */}
